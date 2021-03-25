b0ee1817e30b8f2ffbea1f154a9c9d0e12552cac
Leonardo Cavalcante <leonardo_cavalcante@msn.com>
2021-03-24 23:35:06 -0300

    dispatch event on block added

--- src/components/modules/blockManager.ts
+++ src/components/modules/blockManager.ts
@@ -584,7 +584,9 @@ export default class BlockManager extends Module {
       /**
        * Update current block active input
        */
-      this.currentBlock.updateCurrentInput();
+      if (this.currentBlock) {
+        this.currentBlock.updateCurrentInput();
+      }
 
       return this.currentBlock;
     } else {
